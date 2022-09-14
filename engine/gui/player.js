d610567cee0002715edb7eca36b7b0f2a45d85c5
bloopsoup <77690474+bloopsoup@users.noreply.github.com>
2022-09-14 15:56:19 -0700

    adding movable2D

--- engine/gui/player.js
+++ engine/gui/player.js
@@ -1,3 +1,4 @@
+import Movable2D from './utils/movable2D.js';
 import DeltaTimeRunner from './utils/deltaTimeRunner.js'
 import Spritesheet from './utils/spritesheet.js'
 
@@ -5,58 +6,24 @@ export default class Player {
     constructor(gameWidth, gameHeight) {
         this.gameWidth = gameWidth, this.gameHeight = gameHeight;
 
-        this.width = 200, this.height = 200;
-        this.x = 0, this.vx = 0, this.maxSpeed = 5;
-        this.y = this.gameHeight - this.height, this.vy = 0, this.weight = 0;
-        this.accel = 1
-
         this.dtRunner = new DeltaTimeRunner(20, 1000);
         this.spritesheet = new Spritesheet('player', 200, 200, [9, 7]);
-
-        this.states = [];
-        this.currentState = this.states[0];
+        this.movable = new Movable2D(gameWidth, gameHeight, 200, 200);
     }
-
-    setState(state) {
-        this.currentState = this.states[state];
-        this.currentState.enter();
-    }
-
-    checkCollision() {
-        // need entity list here
-    }
-
-    onGround() { return this.y >= this.gameHeight - this.height; }
 
     handle_event(input) {
-        if (input.includes('ArrowRight') && this.vx < this.maxSpeed) this.vx += this.accel;
-        else if (input.includes('ArrowLeft')  && this.vx > -this.maxSpeed) this.vx -= this.accel;
-        else this.vx = 0;
-
-        if (input.includes('ArrowUp') && this.vy > -this.maxSpeed) this.vy -= this.accel;
-        else if (input.includes('ArrowDown') && this.vy < this.maxSpeed) this.vy +=this.accel;
-        else this.vy = 0;
+        if (input.includes('ArrowRight')) this.movable.incrementVelocityX(true);
+        else if (input.includes('ArrowLeft')) this.movable.incrementVelocityX(false);
+        if (input.includes('ArrowUp')) this.movable.jump();
     }
 
     update(dt) {
         this.dtRunner.deltaTimeUpdate(dt, this.spritesheet.nextFrameInRow);
-
-        this.x += this.vx;
-        if (this.x < 0) this.x = 0;
-        if (this.x > this.gameWidth - this.width) this.x = this.gameWidth - this.width;
-
-        this.y += this.vy;
-        if (!this.onGround()) {
-            this.vy += this.weight;
-            this.spritesheet.setFrameY(1);
-        } else {
-            this.vy = 0;
-            this.spritesheet.setFrameY(0);
-        }
-        if (this.y > this.gameHeight - this.height) this.y = this.gameHeight - this.height;
+        this.movable.update();
     }
 
     draw (context) {
-        this.spritesheet.draw(context, this.x, this.y);
+        const [ x, y ] = this.movable.getPos();  
+        this.spritesheet.draw(context, x, y);
     }
 }
---
+++ engine/gui/utils/movable2D.js
@@ -0,0 +1,51 @@
+export default class Movable2D {
+    /** Supporting basic movement. Bounded by maxWidth and maxHeight. */
+
+    constructor(maxWidth, maxHeight, width, height) {
+        this.maxWidth = maxWidth, this.maxHeight = maxHeight;
+        this.width = width, this.height = height;
+        this.x = 0, this.vx = 0, this.accelerationX = 2, this.decelerationX = 1, this.maxSpeedX = 100;
+        this.y = this.maxHeight - this.height, this.vy = 0, this.accelerationY = 2, this.decelerationY = 1, this.maxSpeedY = 100;
+    }
+
+    getPos() { return [this.x, this.y]; }
+    onGround() { return this.y >= this.maxHeight - this.height; }
+    pastLeftWall() { return this.x < 0; }
+    pastRightWall() { return this.x > this.maxWidth - this.width; }
+    pastGround() { return this.y > this.maxHeight - this.height; }
+
+    jump() { if (this.onGround()) this.vy = -20; }
+    fall() { if (!this.onGround()) this.vy += this.decelerationY; }
+
+    incrementVelocityX(right) {
+        if (right && this.vx < this.maxSpeedX) this.vx += this.accelerationX;
+        else if (!right && this.vx > -this.maxSpeedX) this.vx -= this.accelerationX;
+    }
+    decrementVelocityX() {
+        if (this.vx < 0) this.vx += this.decelerationX;
+        if (this.vx > 0) this.vx -= this.decelerationX;
+    }
+
+    incrementVelocityY(down) {
+        if (down && this.vy < this.maxSpeedY) this.vy += this.accelerationY;
+        else if (!down && this.vy > -this.maxSpeedY) this.vy -= this.accelerationY;
+    }
+    decrementVelocityY() {
+        if (this.vy < 0) this.vy += this.decelerationY;
+        if (this.vy > 0) this.vy -= this.decelerationY;
+    }
+
+    updateX() {
+        this.x += this.vx;
+        if (this.pastLeftWall()) this.x = 0;
+        if (this.pastRightWall()) this.x = this.maxWidth - this.width;
+        this.decrementVelocityX();
+    }
+    updateY() {
+        this.y += this.vy;
+        if (this.pastGround()) this.y = this.maxHeight - this.height;
+        this.fall();
+    }
+
+    update() { this.updateX(); this.updateY(); }
+}
